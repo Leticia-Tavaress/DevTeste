6848ce04eeb319c703e12e12445c34a773d888b5
Fábio de Oliveira Santos <fabio.ols@hotmail.com>
2021-03-15 00:38:07 -0300

    Start commit

---
+++ CadastroEpi/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadastroEpi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var ctx = new Db.Entities())
+            {
+                var empresas = ctx.Empresas.Count();
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
